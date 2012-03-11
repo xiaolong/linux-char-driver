9f4360afb53e0cf45704b8d3645de41ec5593eef
Xiaolong Cheng <xiaolong.ch@gmail.com>
2012-03-10 19:48:26 -0800

    the java test class still fails; couldnt see the semaphore is functioning or not

--- DriverTest/src/Reader.java
+++ DriverTest/src/Reader.java
@@ -1,31 +1,35 @@
 import java.io.*;
+import java.io.RandomAccessFile;
 class Reader extends Thread{
-    
+      
     public Reader(){
 	System.out.println("Reader initialized");
     }
 
     public void run(){
 	int ch;
-	//File file= new File("/dev/simplechar");
-	//FileInputStream fin=null;
-	while(true){
-	    try{/*
-		fin= new FileInputStream(file);
-		while((ch=fin.read())!=-1)
-		    System.out.println((char) ch);
-		    fin.close();*/
-		Runtime.getRuntime().exec("cat /dev/simplechar ");
-	    }catch (Exception e){ }
+	RandomAccessFile fin=null;
+	try{
+	    fin= new RandomAccessFile("/dev/simplechar","rw");
+	    while(true){
+		try{
+		    while((ch=fin.read())!=-1){
+			System.out.println((char) ch);
+			//Thread.sleep(50);
+		    }
+		
+		}catch (Exception e){System.out.println(e);}
+	    }
+	} catch(FileNotFoundException e){
+	    System.out.println("Ooops! didn't find device");
 	}
     }
 
+
     public static void main(String[] args){
-	Reader reader1= new Reader();
-	Reader reader2= new Reader();
-	Reader r3 = new Reader();
-	reader1.start();
-	reader2.start();
-	r3.start();
+	Reader r1= new Reader();
+	Reader r2= new Reader();
+	r1.start();
+	r2.start();
     }
 }
